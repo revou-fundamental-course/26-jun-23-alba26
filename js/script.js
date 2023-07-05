ca794ac838a72dbfbd74def338ca4515b82f4543
Adhitya Laksamana Bayu Adrian <adhityalba@gmail.com>
2023-07-05 20:22:41 +0700

    add slider

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const firstNameInput = document.getElementById("firstNameInput");
+const lastNameInput = document.getElementById("lastNameInput");
+const submitButton = document.getElementById("submitButton");
+
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault()
+    const firstNameValue = firstNameInput.value
+    const lastNameValue = lastNameInput.value
+    nameValidation(firstNameValue, lastNameValue)
+})
+
+function nameValidation(fname, lname) {
+    if (fname == "" || lname == "") {
+        alert("First Name / Last Name empty")
+    } else {
+        updateUI(fname, lname)
+    }
+}
+
+function updateUI(fname, lname) {
+    const firstNameOutput = document.getElementById("firstNameOutput");
+    const lastNameOutput = document.getElementById("lastNameOutput");
+    firstNameOutput.textContent = fname
+    lastNameOutput.textContent = lname
+}
+var slideIndex = 1
+setInterval(function(){
+    plusDiv(1)
+}, 1500)
+function plusDiv(index) {
+    showImage(slideIndex += index)
+}
+
+function showImage(index) {
+    console.log(index)
+    const imgList = document.getElementsByClassName("slider")
+    if (index > imgList.length) { slideIndex = 1}
+    if (index < 1) { slideIndex = imgList.length }
+    for(i = 0; i < imgList.length; i++) {
+        imgList[i].style.display = "none"
+    }
+    imgList[slideIndex - 1].style.display = "block"
+}
